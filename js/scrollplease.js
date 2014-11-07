193a9cb65d444c48f59062da9ec88f534945d83d
Travis Waelbroeck <twaelbroeck@gmail.com>
2014-11-06 23:41:06 -0500

    "Added Content, Basic Layout, No Styling"

---
+++ js/scrollplease.js
@@ -0,0 +1,8 @@
+// When the document is loaded...
+$(document).ready(function()
+{
+	// Scroll the whole document
+	$('.scroll-link').localScroll({		// Wrap link with scroll-link class
+		target:'body'
+	});
+}); 
